6d02c7ce79839f5b92fd458250213532cd99f92e
Guilherme Ribeiro de Souza <guilhermeribeirosouza92@gmail.com>
2024-08-14 09:05:16 -0300

    initial units test

--- src/app/pages/login/login.component.spec.ts
+++ src/app/pages/login/login.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { ToastModule } from 'primeng/toast';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,7 +10,8 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent ],
+      imports: [ToastModule, FormsModule, ReactiveFormsModule]
     })
     .compileComponents();
 
